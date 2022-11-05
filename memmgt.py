54e65a7e897ca48fc54066745c4889b3b3370802
Aditya Mitra <adityaarghya0@gmail.com>
2022-11-05 10:49:15 +0530

    Update memmgt.py

--- memmgt.py
+++ memmgt.py
@@ -4,7 +4,7 @@ import os
 
 def restart():
   mem=int(psutil.virtual_memory().free/1024)
-  print("RAM Usage: ",mem)
+  print("Free RAM: ",mem)
   if mem<92106:
     print("Apache restarted")
     os.system("sudo service apache2 restart")
