41a020f7a323173e77aba32066c5a4505ef40a16
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2022-11-05 15:54:20 +0530

    Update memmgt.py

--- memmgt.py
+++ memmgt.py
@@ -1,9 +1,7 @@
 import psutil
 import gc
 import os
-#from sqloperations import *
 def restart():
-  #getUserCount()
   mem=int(psutil.virtual_memory().free/1024)
   print("Free RAM: ",mem)
   if mem<30000:
