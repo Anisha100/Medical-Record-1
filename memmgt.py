db3f899a61c3d8e55ad2bf09ff1f274a03c51c9d
Aditya Mitra <adityaarghya0@gmail.com>
2022-11-05 15:27:09 +0530

    Update memmgt.py

--- memmgt.py
+++ memmgt.py
@@ -1,10 +1,12 @@
 import psutil
 import gc
 import os
-
+from sqloperations import *
 def restart():
-  mem=int(psutil.virtual_memory().free/1024)
-  print("Free RAM: ",mem)
-  if mem<30000:
-    print("Apache restarted")
-    os.system("sudo service apache2 restart")
+  getUserCount()
+  #mem=int(psutil.virtual_memory().free/1024)
+  #print("Free RAM: ",mem)
+  #if mem<30000:
+    #print("Apache restarted")
+    #os.system("sudo service apache2 restart")
+    
