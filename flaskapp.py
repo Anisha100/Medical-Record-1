ee33f6dcf4c895593048205b44e236ac4fcdfa10
Aditya Mitra <adityaarghya0@gmail.com>
2022-11-05 10:35:39 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -675,16 +675,17 @@ def decr(tok):
 	return f1.decrypt(tok.encode()).decode()
 
 def get_location(ip):
-	response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=62cddacb34cb4dbfb0fc5cba0e329039&ip_address="+ip)
-	json_data = json.loads(response.text)
-	print(json_data)
-	print(ip)
-	city=json_data['city']
-	country=json_data['country']
-	k="City: "+city+"\n"
-	k=k+"Country: "+country+"\n"
-	k=k+"IP Address: "+ip
-	return k
+	#response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=62cddacb34cb4dbfb0fc5cba0e329039&ip_address="+ip)
+	#json_data = json.loads(response.text)
+	#print(json_data)
+	#print(ip)
+	#city=json_data['city']
+	#country=json_data['country']
+	#k="City: "+city+"\n"
+	#k=k+"Country: "+country+"\n"
+	#k=k+"IP Address: "+ip
+	#return k
+	return ""
 
 def save_key(uname, credentials):
 	fln=getFileFromUsername(uname)
